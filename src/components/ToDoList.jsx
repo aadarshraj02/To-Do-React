9d12abc6cfd97197609d7352641e8824e2bfcc72
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 13:49:29 +0530

    Imported ToDoItem.jsx

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,12 @@
+import ToDoItem from "./ToDoItem";
+
 const ToDoList = () => {
   return (
     <div className="py-2 px-4">
       <h1 className="text-zinc-300 text-xl font-semibold text-center">
         All Task Finished Or No Task Added
       </h1>
+      <ToDoItem />
     </div>
   );
 };
