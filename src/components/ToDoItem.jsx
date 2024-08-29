e60bec4118a5c416b44b5acfacf281d36bc2db6c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 13:55:52 +0530

    added layout

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,5 +1,21 @@
+import { MdEditNote } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
 const ToDoItem = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <input type="checkbox" />
+      </div>
+      <div>
+        <h3>Code</h3>
+      </div>
+      <div>
+        <MdEditNote />
+        <MdDelete />
+      </div>
+    </div>
+  );
 };
 
 export default ToDoItem;
