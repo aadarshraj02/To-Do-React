f777f7d9343eb7f7cd3e2348b34861652fd3ac76
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 13:48:01 +0530

    styled ToDoList

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,9 @@
 const ToDoList = () => {
   return (
-    <div>
-      <h1>All Task Finished Or No Task Added</h1>
+    <div className="py-2 px-4">
+      <h1 className="text-zinc-300 text-xl font-semibold text-center">
+        All Task Finished Or No Task Added
+      </h1>
     </div>
   );
 };
