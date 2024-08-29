8c1d3d71c405fab86a8f2609df445a386cfdb4c3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 14:21:01 +0530

    added function to hand;e Submit and add task

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
-const Header = () => {
+import { useState } from "react";
+
+const Header = ({ addTask }) => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      addTask(task);
+      setTask("");
+    }
+  };
+
   return (
     <div className="mt-20 p-4">
-      <form className="flex flex-col md:flex-row gap-3 w-full">
+      <form
+        className="flex flex-col md:flex-row gap-3 w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-col md:flex-row gap-2 w-full items-center">
           <label className="text-zinc-300 text-xl font-semibold">Task</label>
           <input
             type="text"
             className="p-1 md:p-2 rounded-md outline-none md:w-[70%] w-full"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
           />
-          <button className="bg-green-700 px-2 py-1 md:p-2 rounded-md text-zinc-200 hover:text-zinc-300 hover:bg-green-800 transition-all duration-300 ease-linear md:w-[30%] lg:w-[20%] w-full">
+          <button
+            type="submit"
+            className="bg-green-700 px-2 py-1 md:p-2 rounded-md text-zinc-200 hover:text-zinc-300 hover:bg-green-800 transition-all duration-300 ease-linear md:w-[30%] lg:w-[20%] w-full"
+          >
             Add Task
           </button>
         </div>
