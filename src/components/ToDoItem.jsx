361fe099bc4e1d7df3f3ddbf4f57a3d2fd50b901
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 14:24:59 +0530

    passed as props and added task dynamically

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,14 @@
-import { MdEditNote } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
+import React from "react";
+import { MdEditNote, MdDelete } from "react-icons/md";
 
-const ToDoItem = () => {
+const ToDoItem = ({ task }) => {
   return (
     <div className="flex items-center justify-between py-2 px-5 bg-zinc-950 mb-5 rounded-lg">
       <div className="hidden md:block">
-        <input type="checkbox" />
+        <input type="checkbox" checked={task.completed} />
       </div>
       <div>
-        <h3 className="text-white text-lg">Code</h3>
+        <h3 className="text-white text-lg">{task.text}</h3>
       </div>
       <div className="flex gap-5">
         <MdEditNote className="text-blue-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear" />
