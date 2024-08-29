0bd2d4e14384696156393d99645e93a5042e75ee
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 13:43:16 +0530

    imported toDoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import ToDoList from "./components/ToDoList";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <ToDoList />
     </div>
   );
 };
