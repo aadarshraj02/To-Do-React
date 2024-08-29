1978cfcd98619b53d2056856fb7457dc9fe294ad
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 15:11:54 +0530

    added function to edit

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,19 +1,22 @@
 import { MdEditNote, MdDelete } from "react-icons/md";
 
-const ToDoItem = ({ task, deleteTask }) => {
+const ToDoItem = ({ task, deleteTask, startEditing }) => {
   return (
     <div className="flex items-center justify-between py-2 px-5 bg-zinc-950 mb-5 rounded-lg">
       <div className="hidden md:block">
-        <input type="checkbox" checked={task.completed} />
+        <input type="checkbox" checked={task.completed} readOnly />
       </div>
       <div>
         <h3 className="text-white text-lg">{task.text}</h3>
       </div>
       <div className="flex gap-5">
-        <MdEditNote className="text-blue-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear" />
+        <MdEditNote
+          className="text-blue-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear"
+          onClick={() => startEditing(task)}
+        />
         <MdDelete
-          onClick={() => deleteTask(task.id)}
           className="text-red-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear"
+          onClick={() => deleteTask(task.id)}
         />
       </div>
     </div>
