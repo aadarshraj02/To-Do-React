b566d52809a06e284d11a4ebde1120b7f74860b6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 14:47:46 +0530

    added onClick to delete task by id

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,6 +1,6 @@
 import { MdEditNote, MdDelete } from "react-icons/md";
 
-const ToDoItem = ({ task }) => {
+const ToDoItem = ({ task, deleteTask }) => {
   return (
     <div className="flex items-center justify-between py-2 px-5 bg-zinc-950 mb-5 rounded-lg">
       <div className="hidden md:block">
@@ -11,7 +11,10 @@ const ToDoItem = ({ task }) => {
       </div>
       <div className="flex gap-5">
         <MdEditNote className="text-blue-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear" />
-        <MdDelete className="text-red-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear" />
+        <MdDelete
+          onClick={() => deleteTask(task.id)}
+          className="text-red-500 text-xl cursor-pointer hover:scale-125 transition-all duration-300 ease-linear"
+        />
       </div>
     </div>
   );
