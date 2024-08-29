3ce3306fd0979ce36cf04196ae3e8d189c224da3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 14:17:02 +0530

    added useState and passed props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState, useRef } from "react";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 import ToDoList from "./components/ToDoList";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const taskIdRef = useRef(1);
+
+  const addTask = (taskText) => {
+    const newTask = {
+      id: taskIdRef.current,
+      text: taskText,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    taskIdRef.current += 1;
+  };
+
   return (
     <div>
-      <Navbar />
-      <Header />
-      <ToDoList />
+      <Header addTask={addTask} />
+      <ToDoList tasks={tasks} />
     </div>
   );
 };
