7856762f5accd11c9055edd9fee59e2ce72d061e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-29 12:42:25 +0530

    added layout for input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="mt-20">
+      <form>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Header;
